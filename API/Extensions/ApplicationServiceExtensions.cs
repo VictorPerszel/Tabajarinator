215bf61be986a24c4147b944aa4cc2bf19fe566c
Victor Perszel <victorperszel@gmail.com>
2024-04-06 21:21:56 +0100

    Autenticação e JWT

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,25 @@
+
+using API.Data;
+using API.Interfaces;
+using API.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<DataContext>(opt => 
+        {
+            opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
+        });
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddScoped<ITokenService, TokenService>();
+
+        return services;
+        }
+    }
+}
