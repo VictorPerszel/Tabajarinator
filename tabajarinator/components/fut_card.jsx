67fa0d18bcc99b3ac35c301969bcfddee22c57ac
Victor Perszel <victorperszel@gmail.com>
2025-03-30 21:50:57 +0100

    Fix ao avaliar jogador

--- tabajarinator/components/fut_card.jsx
+++ tabajarinator/components/fut_card.jsx
@@ -1,7 +1,22 @@
 import Rating from 'react-rating';
 import { FaStar } from 'react-icons/fa';
+import axios from 'axios';
 
 export default function FUTCard(props) {
+    const handleSubmit = async (avaliadoId, nota) => { 
+        try {     
+            await axios.post('http://localhost:8000/api/avaliacoes/criar/', {     
+                avaliado: avaliadoId,     
+                nota_geral: nota,     
+            }, {     
+                headers: {
+                    Authorization: `Token ${localStorage.getItem('token')}`,      
+                },     
+            });     
+        } catch (error) {     
+            console.error('Erro ao criar avaliação:', error);     
+        }     
+    };  
 
     return (
         <>
@@ -14,9 +29,7 @@ export default function FUTCard(props) {
                         <div class="player-nation"><img src="https://selimdoyranli.com/cdn/fut-player-card/img/argentina.svg" alt="Argentina" draggable="false" /></div>
                         <div class="player-club"><img src="https://selimdoyranli.com/cdn/fut-player-card/img/barcelona.svg" alt="Barcelona" draggable="false" /></div>
                     </div>
-                    <div class="player-picture"><img src="https://selimdoyranli.com/cdn/fut-player-card/img/messi.png" alt="Messi" draggable="false" />
-                        <div class="player-extra"><span>4*SM</span><span>4*WF</span></div>
-                    </div>
+                    <div class="player-picture"><img src="https://selimdoyranli.com/cdn/fut-player-card/img/messi.png" alt="Messi" draggable="false" /> </div>
                 </div>
                 <div class="player-card-bottom">
                     <div class="player-info">
--- tabajarinator/pages/avaliar.js
+++ tabajarinator/pages/avaliar.js
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import Rating from 'react-rating';
-import { FaStar } from 'react-icons/fa';
 import FUTCard from '@/components/fut_card';
-
-
 
 const Avaliar = () => {
   const [avaliacoes, setAvaliacoes] = useState([]);
