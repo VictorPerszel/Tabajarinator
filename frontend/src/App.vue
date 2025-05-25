45ea25a91611863b5436fe62d19ff3a401fb4b88
Victor Perszel <victorperszel@gmail.com>
2025-05-25 12:14:51 +0100

    Leftovers old pc

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<style>
+/* Tailwind CSS */
+@import 'tailwindcss/base';
+@import 'tailwindcss/components';
+@import 'tailwindcss/utilities';
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container mx-auto p-4">
+    <h1 class="text-3xl font-bold underline">
+      Welcome to Tabajarinator!
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
